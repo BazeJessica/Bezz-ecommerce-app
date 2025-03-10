dd3e9af8f1b13dab07af581a9474d69374d1f436
Jessica <jessicaennor@gmail.com>
2025-03-10 11:38:02 +0100

    feat:set up an ecommerce web application

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+@Component({
+  selector: 'app-home',
+  imports: [],
+  template: `
+   <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+    </section>
+  `,
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+
+}
