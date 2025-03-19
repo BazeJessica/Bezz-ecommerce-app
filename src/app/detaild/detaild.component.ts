f46faa9470a477f695aa01ef2c465df10640bb67
Jessica <jessicaennor@gmail.com>
2025-03-19 20:37:34 +0100

    feat: included services

---
+++ src/app/detaild/detaild.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detaild',
+  imports: [],
+  template: `
+    <p>
+      detaild works!
+    </p>
+  `,
+  styles: ``
+})
+export class DetaildComponent {
+
+}
