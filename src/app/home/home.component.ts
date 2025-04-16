40cce80767df73a0bfe5f341b5657141a5278f48
Jessica <jessicaennor@gmail.com>
2025-04-16 16:29:46 +0200

    feat: implemented a filter property

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,13 +9,13 @@ import { Housinglocation } from '../housinglocation';
   template: `
    <section>
       <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter />
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
     <section class="results">
       <app-housing-location
-      *ngFor="let housingLocation of housingLocationList"
+      *ngFor="let housingLocation of filteredLocationList"
        [housingLocation]="housingLocation"></app-housing-location>
     </section>
   `,
@@ -24,7 +24,18 @@ import { Housinglocation } from '../housinglocation';
 
 export class HomeComponent {
   housingLocationList: Housinglocation[] = [];
+filteredLocationList: any;
   constructor(private housingService: HousingService){
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+  filterResults(text:string){
+    if(!text){
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation)=>{
+      return housingLocation?.city.toLowerCase().includes(text.toLowerCase()) ||
+      housingLocation?.state.toLowerCase().includes(text.toLowerCase())    })
   }
 }
